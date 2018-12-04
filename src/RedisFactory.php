72258c0f05645a4bc4bf991d3c2738ef66d7bf08
Anton Griniv <a.griniv@gmail.com>
2018-12-04 18:37:25 +0500

    RedisFactory

---
+++ src/RedisFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AuthService;
+
+use Redis;
+
+class RedisFactory {
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @param Config $config
+     */
+    public function __construct(Config $config) {
+        $this->config = $config;
+    }
+
+    /**
+     * @return Redis
+     */
+    public function create(): Redis {
+        $redis = new Redis();
+        $redis->connect($this->config::get('redis.host'), $this->config::get('redis.port'));
+        return $redis;
+    }
+
+}
